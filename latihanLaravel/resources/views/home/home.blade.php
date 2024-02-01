a08f2953ffa9b1d6bb014a163d40f39e069b7f2a
yudapratamawahyu@gmail.com <yudapratamawahyu@gmail.com>
2024-02-01 10:52:43 +0700

    Tugas 13: Laravel Template

--- latihanLaravel/resources/views/home/home.blade.php
+++ latihanLaravel/resources/views/home/home.blade.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>home</title>
-</head>
-<body>
-    <h1>Selamat Datang Form Home</h1>
-    <ul>
-        <li>List 1</li>
-        <li>List 2</li>
-        <li>List 3 <a href="/form_register">Klik disini</a></li>
-        <li>List 4</li>
-        <li>List 5</li>
-    </ul>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('judul', 'Home')
+
+@section('content')
+<ul>
+    <li>List 1</li>
+    <li>List 2</li>
+    <li>List 3 <a href="/form_register">Klik disini</a></li>
+    <li>List 4</li>
+    <li>List 5</li>
+</ul>
+@endsection
--- latihanLaravel/routes/web.php
+++ latihanLaravel/routes/web.php
@@ -19,3 +19,11 @@ Route::get('/', [HomeController::class, "home"]);
 Route::get('/form_register', [AuthController::class, "register"]);
 
 Route::post('/signup', [AuthController::class, "daftar_register"]);
+
+Route::get('/table', function() {
+    return view('table.table');
+});
+
+Route::get('/data-table', function() {
+    return view('data-table.data-table');
+});
